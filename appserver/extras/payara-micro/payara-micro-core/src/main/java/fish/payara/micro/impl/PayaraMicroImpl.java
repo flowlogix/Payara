027dd40da6b7f6aa98b80c3253268ae0f1a1defc
Steve Millidge <smillidge@users.noreply.github.com>
2017-04-20 19:37:54 +0100

    PAYARA-1586 honour --logtofile in uber jar fixes #1527 (#1540)
    downported

--- appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
+++ appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -1901,6 +1901,7 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
         autoBindSsl = Boolean.getBoolean("payaramicro.autoBindSsl");
         generateLogo = Boolean.getBoolean("payaramicro.logo");
         logToFile = Boolean.getBoolean("payaramicro.logToFile");
+        userLogFile = System.getProperty("payaramicro.userLogFile");
         enableAccessLog = Boolean.getBoolean("payaramicro.enableAccessLog");
         enableAccessLogFormat = Boolean.getBoolean("payaramicro.enableAccessLogFormat");
         logPropertiesFile = Boolean.getBoolean("payaramicro.logPropertiesFile");
@@ -2044,6 +2045,10 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
         props.setProperty("payaramicro.hostAware", Boolean.toString(hostAware));
         props.setProperty("payaramicro.disablePhoneHome", Boolean.toString(disablePhoneHome));
 
+        if (userLogFile != null) {
+            props.setProperty("payaramicro.userLogFile", userLogFile);
+        }
+        
         if (httpPort != Integer.MIN_VALUE) {
             props.setProperty("payaramicro.port", Integer.toString(httpPort));
         }
