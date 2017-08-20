ed93e75c804acc9bcdc46c35a2e1956d5344412d
smillidge <smillidge@c2b2.co.uk>
2017-08-20 20:25:31 +0100

    PAYARA-1966 Fix non-persistent timers with --nocluster

--- appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
+++ appserver/extras/payara-micro/payara-micro-core/src/main/java/fish/payara/micro/impl/PayaraMicroImpl.java
@@ -1665,6 +1665,7 @@ public class PayaraMicroImpl implements PayaraMicroBoot {
         // check hazelcast cluster overrides
         if (noCluster) {
             preBootCommands.add(new BootCommand("set", "configs.config.server-config.hazelcast-runtime-configuration.enabled=false"));
+            preBootCommands.add(new BootCommand("set", "configs.config.server-config.ejb-container.ejb-timer-service.ejb-timer-service=Dummy"));
         } else {
 
             if (hzPort > Integer.MIN_VALUE) {
